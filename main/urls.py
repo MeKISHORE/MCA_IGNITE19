ce9a34ad0fa86cb103a647d48942d9317ca17c55
IGNITE 2019 <ignite@ignite.nitk.ac.in>
2019-09-23 00:06:13 +0000

    url

--- main/urls.py
+++ main/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('nitk_super_admin/', admin.site.urls),
     path('', include('workshop.urls')),
 ]
